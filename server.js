b8b123534da4c8e5457f4439b1f6c1ad1b81736d
Thomas Kim <thomaskim08310@gmail.com>
2024-08-16 10:48:10 -0400

    middleware json

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 import express from 'express'
 const app = express()
+//setup middleware to accept json
+app.use(express.json())
 
 app.get("/", (req, res)=>{
   res.send("hello world")
+})
+
+app.post('/', (req,res)=>{
+  console.log(req)
+  res.json({message: 'data received', data: req.body})
 })
 
 app.listen(5100, ()=>{
