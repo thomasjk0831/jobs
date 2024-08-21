a760e1311d461dc7246901c74317a8d59fece038
Thomas Kim <thomaskim08310@gmail.com>
2024-08-21 13:09:00 -0400

    create

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ dotenv.config()
 import express from 'express'
 const app = express()
 import morgan from 'morgan'
+import { nanoid } from 'nanoid'
+
+const jobs = [
+  {id: nanoid(), company: "IBM", position: "project manager"},
+  {id: nanoid(), company: "Pepsi", position: "Cashier"}
+]
 
 //setup middleware to use logging about requests
 if(process.env.NODE.ENV === 'development')
@@ -13,6 +19,24 @@ app.use(express.json())
 
 app.get("/", (req, res)=>{
   res.send("hello world")
+})
+
+// GET JOBS
+app.get("/jobs", (req, res)=>{
+  res.status(200).json({ jobs })
+})
+
+// CREATE JOB
+app.post('/api/v1/jobs', (req, res) => {
+  const { company, position } = req.body
+  if(!company || !position){
+    return res.status(400).json({msg: 'please provide company and position'})
+  }
+
+  const id = nanoid(10)
+  const job = {id, company, position}
+  jobs.push(job)
+  return res.status(200).json({ job })
 })
 
 app.post('/', (req,res)=>{
