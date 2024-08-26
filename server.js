353b768e3e8b2ae763e03b63f092d56582c371cb
Thomas Kim <thomaskim08310@gmail.com>
2024-08-26 14:17:19 -0400

    test

--- server.js
+++ server.js
@@ -96,3 +96,31 @@ const port = process.env.PORT || 5100
 app.listen(port, ()=>{
     console.log("listening on 5100...")
 })
+
+function inOrderTraverse(tree, array) {
+  if(tree !== null){
+    inOrderTraverse(tree.left, array)
+    array.push(tree.value)
+    inOrderTraverse(tree.right, array)
+  }
+  return array
+}
+
+function preOrderTraverse(tree, array) {
+  if(tree !== null){
+    
+  array.push(tree.value)
+  preOrderTraverse(tree.left, array)
+  preOrderTraverse(tree.right, array)
+  }
+  return array
+}
+
+function postOrderTraverse(tree, array) {
+  if(tree !== null){
+    postOrderTraverse(tree.left, array)
+    postOrderTraverse(tree.right, array)
+    array.push(tree.value)
+  }
+  return array
+}
