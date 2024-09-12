2f67c9b5b186a381e6f18fbd9e7bbfa7171cf50e
Thomas Kim <thomaskim08310@gmail.com>
2024-09-12 10:37:43 -0400

    status libray

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,5 +1,5 @@
-import { nanoid } from 'nanoid'
 import Job from '../models/JobModel.js'
+import {StatusCodes} from 'http-status-codes'
 
 export const getAllJobs = async (req, res)=>{
   const jobs = await Job.find({})
@@ -10,9 +10,9 @@ export const getSingleJob = async (req, res) => {
   const { id } = req.params
   const job = await Job.findById(id)
   if(!job){
-    return res.status(404).json({msg:`no job with ${id}`})
+    return res.status(StatusCodes.NOT_FOUND).json({msg:`no job with ${id}`})
   }
-  res.status(200).json({ job })
+  res.status(StatusCodes.OK).json({ job })
 }
 
 export const editJob = async (req, res) => {
@@ -22,29 +22,29 @@ export const editJob = async (req, res) => {
       new: true
     })
     if(!updatedJob){
-      return res.status(404).json({ msg: `job with ${id} not found`})
+      return res.status(StatusCodes.NOT_FOUND).json({ msg: `job with ${id} not found`})
     }
   
-    res.status(200).json({ msg: 'job modified', job: updatedJob})
+    res.status(StatusCodes.OK).json({ msg: 'job modified', job: updatedJob})
   
 }
 
 export const createJob = async (req, res) => {
     const { company, position } = req.body
     if(!company || !position){
-      return res.status(400).json({msg: 'please provide company and position'})
+      return res.status(StatusCodes.BAD_REQUEST).json({msg: 'please provide company and position'})
     }
   
     const job = await Job.create(req.body)
-    res.status(201).json({ job })
+    res.status(StatusCodes.CREATED).json({ job })
 }
 
 export const deleteJob = async (req,res)=> {
     const { id } = req.params
     const removedJob = await Job.findByIdAndDelete(id)
     if(!removedJob){
-      return res.status(404).json({msg: `no job with id: ${id}`})
+      return res.status(StatusCodes.NOT_FOUND).json({msg: `no job with id: ${id}`})
     }
    
-    res.status(200).json({msg: 'job deleted', job: removedJob})
+    res.status(StatusCodes.OK).json({msg: 'job deleted', job: removedJob})
 }
