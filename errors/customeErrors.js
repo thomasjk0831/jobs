7eeb398be087ec68f6f74c170c3fc8aca4f92ed5
Thomas Kim <thomaskim08310@gmail.com>
2024-09-19 15:01:35 -0400

    more errors

--- errors/customeErrors.js
+++ errors/customeErrors.js
@@ -7,3 +7,27 @@ export class notFoundError extends Error{
         this.statusCode = StatusCodes.NOT_FOUND
     }
 }
+
+export class badRequestError extends Error{
+    constructor(message){
+        super(message)
+        this.name="bad request"
+        this.statusCode = StatusCodes.BAD_REQUEST
+    }
+}
+
+export class unauthenticatedError extends Error{
+    constructor(message){
+        super(message)
+        this.name="unauthenticatedError"
+        this.statusCode = StatusCodes.UNAUTHORIZED
+    }
+}
+
+export class unauthorizedError extends Error{
+    constructor(message){
+        super(message)
+        this.name="unauthorizedError"
+        this.statusCode = StatusCodes.FORBIDDEN
+    }
+}
