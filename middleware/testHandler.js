2456fbbc8fc33eb449ce97dd8b950c30968e3d97
Thomas Kim <thomaskim08310@gmail.com>
2024-10-01 08:47:07 -0400

    test

--- middleware/testHandler.js
+++ middleware/testHandler.js
@@ -7,4 +7,11 @@ const testHandler = (err, req, res, next)=>{
     res.status(statusCode).json({msg})
 }
 
+popupTemplate.content = "<p>As of 2015, <b>{MARRIEDRATE}%</b> of the" +
+" population in this zip code is married.</p>" +
+"<ul><li>{MARRIED_CY} people are married</li>" +
+"<li>{NEVMARR_CY} have never married</li>" +
+"<li>{DIVORCD_CY} are divorced</li><ul>"
+
 export default errorHandler
+
